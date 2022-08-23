ee2c9faa631b38d7cc33a61dd9c6eec0e337e0c3
Ivan Starčević <istarcevic16@gmail.com>
2022-08-23 13:13:49 +0200

    Task 2 - UI za Login i Register stranicu

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,35 +1,66 @@
 import React from "react";
+import { FaQuestionCircle } from "react-icons/fa";
+import { FaSignInAlt } from "react-icons/fa";
 
 const Login = () => {
   return (
-    <div className='container mx-auto p-4 bg-white'>
-      <div className='w-full md:w-1/2 lg:w-1/3 mx-auto my-12'>
-        <h1 className='text-lg font-bold'>Login</h1>
-        <form className='flex flex-col mt-4'>
-          <input
-            type='email'
-            name='email'
-            className='px-4 py-3 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm'
-            placeholder='Email address'
-          />
-          <input
-            type='password'
-            name='password'
-            className='px-4 py-3 mt-4 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm'
-            placeholder='Password'
-          />
-          <button
-            type='submit'
-            className='mt-4 px-4 py-3  leading-6 text-base rounded-md border border-transparent text-white focus:outline-none bg-blue-500 text-blue-100 hover:text-white focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 cursor-pointer inline-flex items-center w-full justify-center items-center font-medium focus:outline-none'>
-            Login
-          </button>
-          <div className='flex flex-col items-center mt-5'>
-            <p className='mt-1 text-xs font-light text-gray-500'>
-              New to Here?
-              <a className='ml-1 font-medium text-blue-400'>Sign up now</a>
-            </p>
+    <div className="min-h-screen flex">
+      <div className="bg-gradient-to-br from-blue-600 hidden to-blue-900 w-4/5 h-screen md:flex justify-center items-center">
+        <img
+          src="../assets/TodoIlustracija.png"
+          alt="Ilustracija za registraciju"
+          className="max-h-screen"
+        />
+      </div>
+      <div className="bg-gradient-to-b from-white to-slate-300 w-full h-screen flex justify-center">
+        {/* form */}
+        <div className="w-[35em] 2xl:min-w-[50em] px-2 flex justify-center flex-col">
+          <div className="bg-white px-6 py-8 rounded shadow-2xl  text-black w-full">
+            <h1 className="mb-8 text-4xl font-semibold text-center text-blue-600">
+              Prijavite se
+            </h1>
+            <form>
+              <input
+                type="text"
+                className="block bg-gray-100 focus:bg-white w-full p-3 rounded mb-4"
+                name="email"
+                placeholder="Email"
+              />
+              <input
+                type="password"
+                className="block bg-gray-100 focus:bg-white w-full p-3 rounded mb-4"
+                name="lozinka"
+                placeholder="Lozinka"
+              />
+              <button
+                type="submit"
+                className="w-full text-center py-3 rounded bg-green text-white font-semibold tracking-wider bg-blue-600 hover:bg-blue-500 focus:outline-none my-1 flex justify-center items-center"
+              >
+                Prijava
+                <FaSignInAlt className="ml-2" />
+              </button>
+              <div className="flex justify-center items-center group  h-full mt-4">
+                <div>
+                  <FaQuestionCircle className="text-gray-500 group-hover:text-blue-400 mr-2" />
+                </div>
+                <div className=" text-gray-600 group-hover:text-blue-400 cursor-pointer">
+                  Zaboravio/la sam lozinku
+                </div>
+              </div>
+            </form>
           </div>
-        </form>
+          <div className="text-gray-900 mt-6">
+            Nemate napravljen profil?
+            <a
+              className="no-underline border-b border-blue text-blue-600 hover:text-blue-500 px-1 font-semibold"
+              href="../register/"
+            >
+              Registrirajte se
+            </a>
+          </div>
+        </div>
+
+        {/* form end */}
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
