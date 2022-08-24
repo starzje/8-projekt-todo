8fca60761eeb6d9da62316104b36f45f95ce5596
Ivan Starčević <istarcevic16@gmail.com>
2022-08-24 23:44:11 +0200

    task 8 - Dashboard UI

--- src/components/Login.js
+++ src/components/Login.js
@@ -41,27 +41,25 @@ const Login = () => {
   };
 
   return (
-    <div className='min-h-screen flex'>
-      <div className='bg-gradient-to-br from-blue-600 hidden to-blue-900 w-4/5 h-screen md:flex justify-center items-center'>
+    <div className="min-h-screen flex">
+      <div className="gradient-blu blue-background ">
         <img
-          src='../assets/TodoIlustracija.png'
-          alt='Ilustracija za registraciju'
-          className='max-h-screen'
+          src="../assets/TodoIlustracija.png"
+          alt="Ilustracija za registraciju"
+          className="max-h-screen"
         />
       </div>
-      <div className='bg-gradient-to-b from-white to-slate-300 w-full h-screen flex justify-center'>
+      <div className="gradient-white white-background">
         {/* form */}
-        <div className='w-[35em] 2xl:min-w-[50em] px-2 flex justify-center flex-col'>
-          <div className='bg-white px-6 py-8 rounded shadow-2xl  text-black w-full'>
-            <h1 className='mb-8 text-4xl font-semibold text-center text-blue-600'>
-              Prijavite se
-            </h1>
+        <div className="form-container">
+          <div className="form-background">
+            <h1 className="form-text">Prijavite se</h1>
             <form onSubmit={handleSubmit}>
               <input
                 ref={usernameInput}
-                type='text'
-                className='block bg-gray-100 focus:bg-white w-full p-3 rounded mb-4'
-                placeholder='Korisnicko ime'
+                type="text"
+                className="form-input"
+                placeholder="Korisnicko ime"
                 value={form.username}
                 onChange={() =>
                   dispatch(
@@ -75,10 +73,10 @@ const Login = () => {
               />
               <input
                 ref={passwordInput}
-                type='password'
-                className='block bg-gray-100 focus:bg-white w-full p-3 rounded mb-4'
-                name='lozinka'
-                placeholder='Lozinka'
+                type="password"
+                className="form-input"
+                name="lozinka"
+                placeholder="Lozinka"
                 value={form.password}
                 onChange={() =>
                   dispatch(
@@ -90,28 +88,24 @@ const Login = () => {
                 }
                 required
               />
-              <button
-                type='submit'
-                className='w-full text-center py-3 rounded bg-green text-white font-semibold tracking-wider bg-blue-600 hover:bg-blue-500 focus:outline-none my-1 flex justify-center items-center'>
+              <button type="submit" className="login-btn">
                 Prijava
-                <FaSignInAlt className='ml-2' />
+                <FaSignInAlt className="ml-2" />
               </button>
-              <div className='flex justify-center items-center group  h-full mt-4'>
+              <div className="flex-util group  h-full mt-4">
                 <div>
-                  <FaQuestionCircle className='text-gray-500 group-hover:text-blue-400 mr-2' />
+                  <FaQuestionCircle className="text-gray-500 group-hover:text-blue-400 mr-2" />
                 </div>
-                <div className=' text-gray-600 group-hover:text-blue-400 cursor-pointer'>
+                <div className=" text-gray-600 group-hover:text-blue-400 cursor-pointer">
                   Zaboravio/la sam lozinku
                 </div>
               </div>
             </form>
-            <div className='text-red-500 mb-4 ml-1' ref={errorFeedback}></div>
+            <div className="text-red-500 mb-4 ml-1" ref={errorFeedback}></div>
           </div>
-          <div className='text-gray-900 mt-6'>
+          <div className="text-gray-900 mt-6">
             Nemate napravljen profil?
-            <a
-              className='no-underline border-b border-blue text-blue-600 hover:text-blue-500 px-1 font-semibold'
-              href='../register/'>
+            <a className="text-highlight" href="../register/">
               Registrirajte se
             </a>
           </div>
